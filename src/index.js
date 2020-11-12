bcb5df752740d2eb8ff0c23a38af7b11e580fe23
Evan Doyle <20085432@mail.wit.com>
2020-11-12 13:15:37 +0000

    Fix buttons on movie list pages

--- src/index.js
+++ src/index.js
@@ -8,24 +8,31 @@ import MoviePage from './pages/movieDetailsPage'
 import FavoriteMoviesPage from './pages/favoritesMoviesPage'
 import MovieReviewPage from "./pages/movieReviewPage";
 import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
+import MoviesContextProvider from "./contexts/moviesContext";
+import GenresContextProvider from "./contexts/genresContext";
+
 const App = () => {
   return (
-      <BrowserRouter>
-        <div className="jumbotron">
-          <SiteHeader />      {/* New Header  */}
-          <div className="container-fluid">
-          <Switch>
+    <BrowserRouter>
+      <div className="jumbotron">
+        <SiteHeader /> 
+        <div className="container-fluid">
+          <MoviesContextProvider>
+            <GenresContextProvider>    {/* NEW */}
+              <Switch>
             <Route path="/reviews/:id" component={MovieReviewPage} />
             <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
             <Route exact path="/movies/upcoming" component={UpcomingMoviesPage} />
             <Route path="/movies/:id" component={MoviePage} />
             <Route path="/" component={HomePage} />
             <Redirect from="*" to="/" />
-          </Switch>
+            </Switch>
+            </GenresContextProvider>    {/* NEW */}
+          </MoviesContextProvider>
         </div>
       </div>
     </BrowserRouter>
-    );
-  };
+  );
+};
   
-  ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
