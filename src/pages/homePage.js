f933f5aefb7bb9f5a4fae39b228bb0ae3a8e8d5d
Evan Doyle <20085432@mail.wit.com>
2020-11-12 11:45:37 +0000

    Show movie review excerpts; Refactor movie page

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,3 +1,4 @@
+import { getMovies } from "../api/tmdb-api";
 import React, { useState, useEffect } from "react";
 import StubAPI from "../api/stubAPI";
 import PageTemplate from '../components/templateMovieListPage'
@@ -5,14 +6,9 @@ import PageTemplate from '../components/templateMovieListPage'
 const MovieListPage = () => {
   const [movies, setMovies] = useState([]);
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
-    )
-      .then(res => res.json())
-      .then(json => json.results )
-      .then(movies => {
-        setMovies(movies);
-      });
+    getMovies().then(movies => {
+      setMovies(movies);
+    });
   }, []);
 
   const addToFavorites = movieId => {
