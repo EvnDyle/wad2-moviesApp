a271790358ea590faffbe94799de95f817b7aea7
Evan Doyle <20085432@mail.wit.com>
2020-11-12 03:11:04 +0000

    Add to favorites logic completed; Dummy page

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -4,6 +4,9 @@ import MovieList from "../components/movieList";
 import FilterControls from "../components/filterControls";
 
 const MovieListPage = () => {
+    const [titleFilter, setTitleFilter] = useState("");       // NEW
+    const [genreFilter, setGenreFilter] = useState("0");      // NEW
+
     const [movies, setMovies] = useState([]);
   useEffect(() => {
     fetch(
@@ -18,11 +21,26 @@ const MovieListPage = () => {
         setMovies(movies);
       });
   }, []);
-  return (
+  
+    const genre = Number(genreFilter)
+    let displayedMovies = movies
+        .filter(m => {
+            return m.title.toLowerCase().search(titleFilter.toLowerCase()) !== -1;
+        })
+        .filter(m => {
+            return genre > 0 ? m.genre_ids.includes(Number(genreFilter)) : true;
+        });
+
+    const handleFilterChange = (type, value) => {
+        if (type === "name") setTitleFilter(value);
+        else setGenreFilter(value);
+    };
+    // NEW BLOCK END
+    return (
     <>
-      <Header numMovies={movies.length} />
-      <FilterControls />
-      <MovieList movies={movies} />
+      <Header numMovies={displayedMovies.length} />          {/* CHANGED */}
+      <FilterControls onUserInput={handleFilterChange} />    {/* CHANGED */}
+      <MovieList movies={displayedMovies} />                  {/* CHANGED */}
     </>
   );
 };
