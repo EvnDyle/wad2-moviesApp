00444ba4a0afb3cb7b7cfb18702cf2d2c5a79af4
Evan Doyle <20085432@mail.wit.com>
2021-08-16 20:05:49 +0100

    Added filtering of movie list - REPEAT

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -4,8 +4,11 @@ import MovieList from "../components/movieList";
 import FilterControls from "../components/filterControls";
 
 const MovieListPage = () => {
-    const [movies, setMovies] = useState([]);
-    useEffect(() => {
+  const [titleFilter, setTitleFilter] = useState("");       // NEW
+  const [genreFilter, setGenreFilter] = useState("0");      // NEW
+
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
       fetch(
         `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
       )
@@ -18,11 +21,27 @@ const MovieListPage = () => {
           setMovies(movies);
         });
     }, []);
+
+    const genre = Number(genreFilter)
+  let displayedMovies = movies
+    .filter(m => {
+      return m.title.toLowerCase().search(titleFilter.toLowerCase()) !== -1;
+    })
+    .filter(m => {
+      return genre > 0 ? m.genre_ids.includes(Number(genreFilter)) : true;
+    });
+
+  const handleFilterChange = (type, value) => {
+    if (type === "name") setTitleFilter(value);
+    else setGenreFilter(value);
+  };
+
+
     return (
     <>
-      <Header numMovies={movies.length} />
-      <FilterControls />
-      <MovieList movies={movies} />
+      <Header numMovies={displayedMovies.length} /> 
+      <FilterControls onUserInput={handleFilterChange} />
+      <MovieList movies={displayedMovies} />
     </>
   );
 };
